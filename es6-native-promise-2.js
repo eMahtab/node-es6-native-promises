9d20e8e8f92f96a7bd8de9246da887511a55098e
Mahtab Alam <alammahtab08@gmail.com>
2017-06-01 13:43:31 +0530

    Added request error

--- es6-native-promise-2.js
+++ es6-native-promise-2.js
@@ -1,16 +1,25 @@
 var http=require('http');
 
 var getRanking=function(url){
-	return new Promise(function(resolve,reject){
-		http.get(url,function(response){
+	var promise= new Promise(function(resolve,reject){
+	var req=http.get(url,function(response){
             if(response.statusCode < 200 || response.statusCode > 299){
             	reject(new Error('ErrorCode '+response.statusCode))
             }
             var result="";
             response.on('data',function(chunk){result +=chunk;} )
             response.on('end',function(){resolve(result);} )
-		})
+		});
+
+	req.on('error',function(err){
+      console.error('Error with the request:', err.message); 
+      reject(err.message); 
+     });
+
+    req.end();
+
 	});
+	return promise;
 }
 
 getRanking('http://localhost:3000/olympic/2016/ranking/4')
--- es6-native-promise-3.js
+++ es6-native-promise-3.js
@@ -2,21 +2,30 @@ var http=require('http');
 var URL='http://localhost:3000';
 
 var getPromise=function(url){
-	var promise=new Promise(function(resolve,reject){
-		http.get(url,function(response){
+	var promise= new Promise(function(resolve,reject){
+   var req=http.get(url,function(response){
             if(response.statusCode < 200 || response.statusCode > 299){
-            	reject(new Error('ErrorCode '+response.statusCode))
+               reject(new Error('ErrorCode '+response.statusCode))
             }
             var result="";
             response.on('data',function(chunk){result +=chunk;} )
             response.on('end',function(){resolve(result);} )
-		})
-	});
+      });
+
+   req.on('error',function(err){
+      console.error('Error with the request:', err.message); 
+      reject(err.message); 
+     });
+
+   req.end();
+
+   });
+
    return promise;
 }
 
 
-getPromise(URL+'/olympic/2016/ranking/4')
+getPromise(URL+'/olympic/2016/ranking/42')
       .then(function(data){
          console.log("Response 1 "+data);
          return getPromise(URL+'/iso/country/'+JSON.parse(data).Country);
